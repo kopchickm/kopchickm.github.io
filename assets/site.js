33c2d9c59907edcbc00c141206e74b1e256ae34b
Michael Kopchick <kopchickm@gmail.com>
2020-02-07 11:46:45 -0500

    Build out MFD and finish initial site

---
+++ assets/site.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  document.getElementById("navigation-toggle").onclick = function() {
+    const menu = document.getElementById("nav-menu");
+    menu.classList.toggle("open")
+  }
+}
